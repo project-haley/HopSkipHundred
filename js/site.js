c2dd4fa74ef6465d042fa0cb15ef9e2b4895568a
project-haley <60005425+project-haley@users.noreply.github.com>
2021-08-15 23:25:35 -0500

    style changes and logo additions

--- js/site.js
+++ js/site.js
@@ -3,3 +3,18 @@
 function HelloWorld() {
     alert("Hello World!")
 }
+
+
+
+function hopSkipHundred(startValue, endValue) {
+    startValue = document.getElementById('startValue');
+    endValue = document.getElementById('endValue');
+
+    for (let i = startValue; i < endValue; i++) {
+        if (i % 2 == 0) {
+            // print i bold
+        } else {
+            // print i normal
+        }
+    }
+}
